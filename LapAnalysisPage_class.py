6df2eb8e0d537faeb92fcd8b419c0f537bdb5698
Pierre BAGNARA <pierre.bagnara@ecole-air.fr>
2026-03-05 20:31:29 +0100

    Added throttle, brake and steering angle graphs to LapAnalysisPAge

--- LapAnalysisPage_class.py
+++ LapAnalysisPage_class.py
@@ -1,7 +1,6 @@
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_daq as daq
 import plotly
 
 from coordinates_handler import Origin, get_sections_from_ini_file, Section, dx, dy
@@ -10,10 +9,12 @@ from coordinates_handler import Origin, get_sections_from_ini_file, Section, dx,
 class LapAnalysisPage:
     def __init__(self, app):
         self._app = app
-        self.track_map_figure = plotly.graph_objects.Figure()
         self.track_map_origin = Origin
         self.track_map_origin.setup("config/reference_points.txt")
-        self.throttle_brake_figure = plotly.graph_objects.Figure()
+        self.track_map_figure = plotly.graph_objects.Figure()
+        self.throttle_figure = plotly.graph_objects.Figure()
+        self.brake_figure = plotly.graph_objects.Figure()
+        self.steering_angle_figure = plotly.graph_objects.Figure()
         self.gg_graph_figure = plotly.graph_objects.Figure()
         self.sections = get_sections_from_ini_file()
         self.selected_section: Section | None = None
@@ -27,31 +28,98 @@ class LapAnalysisPage:
             maxHeight=400,
             placeholder="Sélectionner un secteur",
         )
+
+        self.setup_layout()
         self.setup_callbacks()
+
+        throttle_brake_component = dbc.Col([
+            dbc.Row([
+                dash.dcc.Graph(
+                    figure=self.throttle_figure,
+                    id='throttle-figure',
+                )],
+            ),
+            dbc.Row([
+                dash.dcc.Graph(
+                    figure=self.brake_figure,
+                    id='brake-figure',
+                )],
+            ),
+            dbc.Row([
+                dash.dcc.Graph(
+                    figure=self.steering_angle_figure,
+                    id='steering-angle-figure',
+                )],
+            ),
+        ],
+            width=True,
+        )
 
         self.page = dash.html.Div([
             dash.html.H3('Analyse tour-par-tour'),
             self.section_dropdown,
-            dash.dcc.Graph(
-                figure=self.track_map_figure,
-                id='track-map-figure',
-            ),
+            dbc.Row([
+                dbc.Col([
+                    dash.dcc.Graph(
+                        figure=self.track_map_figure,
+                        id='track-map-figure',
+                    )],
+                    width=6),
+                throttle_brake_component,
+            ])
         ])
+
+    def setup_layout(self):
+        self.track_map_figure.update_layout(
+            yaxis=dict(scaleanchor="x", scaleratio=1),
+            margin=dict(l=10, r=10, t=30, b=20),
+            title=dict(text="Trajectoire")
+        )
+        self.throttle_figure.update_layout(
+            yaxis=dict(range=[0, 100]),
+            height=150,
+            margin=dict(l=10, r=10, t=30, b=30),
+            title=dict(text="Accélérateur (%)")
+        )
+        self.brake_figure.update_layout(
+            yaxis=dict(range=[0, 100]),
+            height=150,
+            margin=dict(l=10, r=10, t=30, b=30),
+            title=dict(text="Frein (%)")
+        )
+        self.steering_angle_figure.update_layout(
+            # yaxis=dict(range=[0, 100]),
+            height=150,
+            margin=dict(l=10, r=10, t=30, b=30),
+            title=dict(text="Angle au volant (°)")
+        )
+
+        # self.gg_graph_figure.update_layout(
+        #     height=175,
+        #     width=175,
+        #     margin=dict(l=10, r=10, t=10, b=10),
+        # )
 
     def setup_callbacks(self):
         self._app.app.callback(
             [
                 dash.dependencies.Output("track-map-figure", 'figure'),
+                dash.dependencies.Output("throttle-figure", 'figure'),
+                dash.dependencies.Output("brake-figure", 'figure'),
+                dash.dependencies.Output("steering-angle-figure", 'figure'),
                 dash.dependencies.Input("dropdown-sector_selection", 'value'),
-             ])(self.update_track_map)
+             ])(self.update_page)
 
-
-    def setup(self):
-        self.gg_graph_figure.update_layout(
-            height=175,
-            width=175,
-            margin=dict(l=10, r=10, t=10, b=10),
-        )
+    def update_page(self, section_name: str):
+        try:
+            self.selected_section = [section for section in self.sections if section.title == section_name][0]
+        except IndexError:
+            self.selected_section = None
+        self.update_track_map()
+        self.update_throttle()
+        self.update_brake()
+        self.update_steering_angle()
+        return self.track_map_figure, self.throttle_figure, self.brake_figure, self.steering_angle_figure
 
     def plot_background(self):
         if self.selected_section is None:
@@ -74,25 +142,16 @@ class LapAnalysisPage:
             xanchor="left",
             yanchor="bottom",
         )
-        # self.track_map_figure.update_xaxes(range=[self.selected_section.top_left.x, self.selected_section.top_left.x + width])
-        # self.track_map_figure.update_yaxes(range=[self.selected_section.bottom_right.y, self.selected_section.bottom_right.y + height])
 
-    def update_track_map(self, section_name: str):
-        self.track_map_figure.data = []
-        self.track_map_figure.layout.images = []
-        try:
-            self.selected_section = [section for section in self.sections if section.title == section_name][0]
-            self.plot_background()
-        except IndexError:
-            self.selected_section = None
-
+    def plot(self, figure: plotly.graph_objs.Figure, x_field: str, y_field: str):
+        sections_extend = 0.02
         if self.selected_section is None:
             for driver, laps in self._app.selected_laps.items():
                 for lap in laps:
                     lap_data = self._app.data[driver][lap.number][10:-10]
-                    x_data = lap_data.loc[:, "Car Coord X (m)\r\r\n"]
-                    y_data = lap_data.loc[:, "Car Coord Y (m)\r\r\n"]
-                    self.track_map_figure.add_trace(
+                    x_data = lap_data.loc[:, x_field]
+                    y_data = lap_data.loc[:, y_field]
+                    figure.add_trace(
                         plotly.graph_objects.Scatter(
                             x=x_data,
                             y=y_data,
@@ -103,19 +162,52 @@ class LapAnalysisPage:
             for driver, laps in self._app.selected_laps.items():
                 for lap in laps:
                     lap_data = self._app.data[driver][lap.number][10:-10]
-                    section_data = lap_data[lap_data["Car Pos Norm\r\r\n"] > self.selected_section.start]
-                    section_data = section_data[section_data["Car Pos Norm\r\r\n"] < self.selected_section.stop]
-                    x_data = section_data.loc[:,"Car Coord X (m)\r\r\n"]
-                    y_data = section_data.loc[:, "Car Coord Y (m)\r\r\n"]
-                    self.track_map_figure.add_trace(
+                    section_data = lap_data[lap_data["Car Pos Norm\r\r\n"] > self.selected_section.start - sections_extend]
+                    section_data = section_data[section_data["Car Pos Norm\r\r\n"] < self.selected_section.stop + sections_extend]
+                    x_data = section_data.loc[:, x_field]
+                    y_data = section_data.loc[:, y_field]
+                    figure.add_trace(
                         plotly.graph_objects.Scatter(
                             x=x_data,
                             y=y_data,
                             name=f"{driver} - L{lap.number} - {lap}",
                         )
                     )
-        self.track_map_figure.update_yaxes(scaleanchor="x", scaleratio=1)
-        return [self.track_map_figure]
+        return figure
+
+    def update_track_map(self):
+        self.track_map_figure.data = []
+        self.track_map_figure.layout.images = []
+        self.plot_background()
+        self.plot(
+            self.track_map_figure,
+            x_field="Car Coord X (m)\r\r\n",
+            y_field="Car Coord Y (m)\r\r\n",
+        )
+
+    def update_throttle(self):
+        self.throttle_figure.data = []
+        self.plot(
+            self.throttle_figure,
+            x_field="Car Pos Norm\r\r\n",
+            y_field="Throttle Pos (%)\r\r\n",
+        )
+
+    def update_brake(self):
+        self.brake_figure.data = []
+        self.plot(
+            self.brake_figure,
+            x_field="Car Pos Norm\r\r\n",
+            y_field="Brake Pos (%)\r\r\n",
+        )
+
+    def update_steering_angle(self):
+        self.steering_angle_figure.data = []
+        self.plot(
+            self.steering_angle_figure,
+            x_field="Car Pos Norm\r\r\n",
+            y_field="Steering Angle (deg)\r\r\n",
+        )
 
     def get_page(self):
         if self.selected_section is None:
@@ -123,117 +215,3 @@ class LapAnalysisPage:
         else:
             self.section_dropdown.value = self.selected_section.title
         return self.page
-
-
-def void():
-    figure_track_map = plotly.graph_objects.Figure()
-    figure_throttle_brake = plotly.graph_objects.Figure()
-    figure_gg_graph = plotly.graph_objects.Figure()
-    figure_gg_graph.update_layout(
-        height=175,
-        width=175,
-        margin=dict(l=10, r=10, t=10, b=10),
-        )
-    options = [dict(label="Tour complet", value="full_lap")]
-    for section in sections:
-        options.append(dict(label=section.title, value=section.title))
-    output = dash.html.Div(
-        [
-            dash.html.H3('Analyse tour-par-tour'),
-            dash.dcc.Dropdown(
-                options=options,
-                id='dropdown-sector_selection',
-                maxHeight=400,
-                placeholder="Sélectionner un secteur",
-            ),
-            dash.dcc.Slider(
-                id='slider-time-scale',
-                min=0,
-                max=1,
-                value=0,
-                ),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        [
-                            dash.dcc.Graph(
-                                figure=figure_track_map,
-                                id='graph-track_map',
-                                ),
-                        ],
-                        width=6,
-                        ),
-                    dbc.Col(
-                        [
-                            dbc.Row(
-                                [
-                                    dbc.Col(
-                                        [
-                                            daq.GraduatedBar(
-                                                id='bar-throttle',
-                                                vertical=True,
-                                                min=0,
-                                                max=100,
-                                                value=25,
-                                                showCurrentValue=True,
-                                                label='Throttle',
-                                                color='green',
-                                                ),
-                                        ]),
-                                    dbc.Col(
-                                        [
-                                            daq.GraduatedBar(
-                                                id='bar-brake',
-                                                vertical=True,
-                                                min=0,
-                                                max=100,
-                                                value=25,
-                                                showCurrentValue=True,
-                                                label='Brake',
-                                                color='red'
-                                                ),
-                                        ]),
-                                    dbc.Col(
-                                        [
-                                            daq.GraduatedBar(
-                                                id='bar-clutch',
-                                                vertical=True,
-                                                min=0,
-                                                max=100,
-                                                value=25,
-                                                showCurrentValue=True,
-                                                label='Clutch',
-                                                color="#9B51E0",
-                                                ),
-                                        ]),
-                                ]),
-                            dbc.Row(
-                                [
-                                    dbc.Col(
-                                        [
-                                            daq.LEDDisplay(
-                                                id='LED-gear',
-                                                label='GEAR',
-                                                value=0,
-                                                ),
-                                        ]),
-                                    dbc.Col(
-                                        [
-                                            dash.dcc.Graph(
-                                                figure=figure_gg_graph,
-                                                id='graph-gg-display',
-                                                ),
-                                        ]),
-                                ]),
-                        ],
-                        width=3,
-                        ),
-                ]),
-            dash.dcc.Graph(
-                figure=figure_throttle_brake,
-                id='graph-throttle-brake-display',
-                ),
-        ],
-        className='dbc dbc-ag-grid',
-    )
-    return output
